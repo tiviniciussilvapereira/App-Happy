5eef0e1a7989dc9ecd4f2dd314ca56a188b91177
Vinicius Silva Pereira <ti.viniciussilvapereira@gmail.com>
2020-10-14 15:28:12 -0300

    Criação do componente para abstrair a sidebar

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,13 +1,23 @@
 import React from 'react'; 
 import {Link} from 'react-router-dom';
-import {FiPlus} from 'react-icons/fi';
-import {Map, TileLayer} from 'react-leaflet';
+import {FiPlus, FiArrowRight} from 'react-icons/fi';
+import {Map, TileLayer, Marker, Popup} from 'react-leaflet';
+import Leaflet from 'leaflet'; 
 
 import 'leaflet/dist/leaflet.css';
 
 import mapMarkerImg from '../images/map-marker.svg'; 
 
 import '../styles/pages/orphanages-map.css'; 
+
+const mapIcon = Leaflet.icon({
+    iconUrl: mapMarkerImg, 
+    iconSize: [58, 68], 
+    iconAnchor: [29,68], 
+    popupAnchor: [170, 2],
+
+});
+
 
 function OrphanagesMap(){
     return(
@@ -38,10 +48,23 @@ function OrphanagesMap(){
             
             <TileLayer url = {`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
             />
+
+            <Marker 
+                icon={mapIcon}
+                position={[-20.5367409,-47.4000602]}>
+                <Popup closeButton={false} minWidth={240} maxWidth={240} className="map-popup">
+                    Lar das meninas
+                    <Link to="/orphanages/1">
+                        <FiArrowRight size={20} color="#FFF">
+
+                        </FiArrowRight>
+                    </Link>
+                </Popup>
+            </Marker>
                 
             </Map>
 
-            <Link to="" className="create-orphanage">
+            <Link to="/orphanages/create" className="create-orphanage">
                 <FiPlus size={32} color="#FFF" />
             </Link>
 
